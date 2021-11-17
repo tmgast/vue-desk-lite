ab211c5c53e3cdad0e162b2e7ae38574e7fd7f41
= <tmgast@gmail.com>
2021-11-18 07:10:21 +0900

    component starters added

---
+++ src/components/Shelf.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="shelf">
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style type="scss">
+  .shelf{
+    top: 0;
+    position: absolute;
+    height: 30px;
+    width: 100%;
+
+    color: #f8f8f8;
+    box-shadow: 5px 5px 5 #aaa;
+  }
+</style>
